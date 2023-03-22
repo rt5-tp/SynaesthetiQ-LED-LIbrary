3e8c2c1c813db1a51db21b306a70edf74f2fcd39
Thomas Nordentoft <thomas@nordentoft.dk>
2023-03-22 13:37:04 +0000

    Expanded test suite resulting in fixes for Colour.hpp

--- test/TestColour.cpp
+++ test/TestColour.cpp
@@ -15,6 +15,26 @@ TEST(getRGBTest, ReturnsConstructorValues){
     EXPECT_EQ(colour.getBlue(), blue);
 }
 
+// Tests the returned GRB.
+TEST(getRGBTest, ReturnsCorrectGRB){
+    Colour colour(0x000e53b8);
+    EXPECT_EQ(colour.getGRB(), 0x00530eb8);
+}
+
+// Tests the returned RGB.
+TEST(getRGBTest, ReturnsCorrectRGB){
+    Colour colour(0x000e53b8);
+    EXPECT_EQ(colour.getRGB(), 0x000e53b8);
+}
+
+TEST(getRGBTest, ConstructorRGBWorks){
+
+    Colour colour(0x000e35b8);
+    EXPECT_EQ(colour.getRed(), 14);
+    EXPECT_EQ(colour.getGreen(), 53);
+    EXPECT_EQ(colour.getBlue(), 184);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
